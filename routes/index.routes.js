53f01cbe19503a8c200939410f1c9b1e600060ea
rkarmoezaw <rkarmoezaw.dev@gmail.com>
2026-05-15 23:30:39 +0800

    fix vocab route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,6 @@ import { Router } from 'express';
 import VocabularyRouter from './Vocabulary.routes.js';
 
 const router = Router();
-router.use('/api/v1/', VocabularyRouter);
+router.use('/api/v1', VocabularyRouter);
 
 export default router;
