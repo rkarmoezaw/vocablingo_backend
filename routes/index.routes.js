2d98d86c00f063901dee1c95628cfd74074e080b
rkarmoezaw <rkarmoezaw.dev@gmail.com>
2026-05-15 23:29:15 +0800

    fix index route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
+import VocabularyRouter from './Vocabulary.routes.js';
 
 const router = Router();
-router.use('api/v1/', AuthRouter);
+router.use('/api/v1/', VocabularyRouter);
 
 export default router;
