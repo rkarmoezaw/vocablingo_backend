93502d1b8184c20459c1796f47d22c3962314bc0
rkarmoezaw <rkarmoezaw.dev@gmail.com>
2026-05-19 01:48:35 +0800

    add putch on cors

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   }),
 );
